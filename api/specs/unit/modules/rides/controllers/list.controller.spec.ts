c2d8d20ee6605526db461302e4952356124016d3
Yuri Pinheiro <yuripinheirot@gmail.com>
2024-11-21 10:50:11 -0300

    test: add validation params to list rides

---
+++ api/specs/unit/modules/rides/controllers/list.controller.spec.ts
@@ -0,0 +1,73 @@
+import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import { TestingModule } from '@nestjs/testing';
+import { buildTestingModule } from '@specs/support/specs.module';
+import { configurePipes } from '@specs/support/configure-pipes';
+import { driversMock } from '@specs/mocks/drivers.mock';
+import { PrismaService } from '@/services/prisma.service';
+import { randomUUID } from 'crypto';
+
+describe('[UNIT] [rides/list.controller] - [list()]', () => {
+  let app: INestApplication;
+  const makeSut = (
+    params: { customer_id: string; driver_id?: string } = {
+      customer_id: randomUUID(),
+    },
+  ) => {
+    const { customer_id, driver_id } = params;
+
+    return `/rides/${customer_id} ${
+      driver_id ? `?driver_id=${driver_id}` : ''
+    }`;
+  };
+
+  beforeAll(async () => {
+    const module: TestingModule = await buildTestingModule()
+      .overrideProvider(PrismaService)
+      .useValue({
+        driver: {
+          findMany: jest.fn().mockResolvedValue(driversMock),
+        },
+      })
+      .compile();
+    app = configurePipes(module.createNestApplication());
+
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+    app = undefined;
+  });
+
+  describe('handle()', () => {
+    describe('validations', () => {
+      test('should validate all required params', async () => {
+        const invalidSut = makeSut({ customer_id: '1' });
+        const response = await request(app.getHttpServer()).get(invalidSut);
+
+        expect(response.status).toBe(400);
+        expect(response.body).toEqual({
+          error: 'Bad Request',
+          message: ['customer_id must be a UUID'],
+          statusCode: 400,
+        });
+      });
+
+      test('should validate all optional params', async () => {
+        const invalidSut = makeSut({
+          customer_id: randomUUID(),
+          driver_id: '1',
+        });
+        const response = await request(app.getHttpServer()).get(invalidSut);
+
+        expect(response.status).toBe(400);
+        expect(response.body).toEqual({
+          error: 'Bad Request',
+          message: ['driver_id must be a UUID'],
+          statusCode: 400,
+        });
+      });
+    });
+  });
+});
