f3e85718ec5d4f62a664f1a4f48b7ca9bb3b6811
Yuri Pinheiro <yuripinheirot@gmail.com>
2024-11-21 10:02:46 -0300

    test: should return an error if the driver is not found

---
+++ api/specs/unit/modules/rides/services/confirm.service.spec.ts
@@ -0,0 +1,81 @@
+import { ConfirmRequestDto } from '@/modules/rides/dtos/confirm.request.dto';
+import { RideRepositoryService } from '@/modules/rides/repository/ride-repository.service';
+import { ConfirmService } from '@/modules/rides/services/confirm.service';
+import { CodeErrorsEnum } from '@/protocols/code-errors.type';
+import { GoogleApiService } from '@/providers/google-api/google-api.service';
+import { formatResponseError } from '@/utils/format-response-error.util';
+import { NotFoundException } from '@nestjs/common';
+import { TestingModule } from '@nestjs/testing';
+import { driversMock } from '@specs/mocks/drivers.mock';
+import { buildTestingModule } from '@specs/support/specs.module';
+
+describe('[UNIT] [rides/confirm.service] - [handle()]', () => {
+  let sut: ConfirmService;
+  let googleApiService: GoogleApiService;
+  let rideRepository: RideRepositoryService;
+
+  beforeAll(async () => {
+    const module: TestingModule = await buildTestingModule().compile();
+
+    sut = module.get<ConfirmService>(ConfirmService);
+
+    googleApiService = module.get<GoogleApiService>(GoogleApiService);
+    rideRepository = module.get<RideRepositoryService>(RideRepositoryService);
+
+    jest
+      .spyOn(rideRepository, 'getDriversByMinDistance')
+      .mockResolvedValue(driversMock);
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const validPayload: ConfirmRequestDto = {
+    customer_id: '1',
+    driver: {
+      id: '1',
+      name: 'John Doe',
+    },
+    origin: 'origin',
+    destination: 'destination',
+    distance: 10,
+    duration: '10s',
+    value: 10,
+  };
+
+  describe('handle()', () => {
+    describe('validations', () => {
+      test('should return an error if the driver is not found', async () => {
+        jest
+          .spyOn(rideRepository, 'findDriverById')
+          .mockResolvedValueOnce(null);
+
+        const invalidDriverId = 'invalid-driver-id';
+
+        const response = sut.handle({
+          ...validPayload,
+          driver: {
+            id: invalidDriverId,
+            name: 'John Doe',
+          },
+        });
+
+        await expect(response).rejects.toThrow(
+          new NotFoundException(
+            formatResponseError({
+              code: CodeErrorsEnum.DRIVER_NOT_FOUND,
+              message: 'Driver not found',
+            }),
+          ),
+        );
+      });
+
+      describe('success', () => {
+        test('should return the drivers', async () => {
+          expect(true).toBe(true);
+        });
+      });
+    });
+  });
+});
