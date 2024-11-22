cb214e86c7209890187d3b66d402234d39c944b7
Yuri Pinheiro <yuripinheirot@gmail.com>
2024-11-22 11:08:43 -0300

    refactor: add class validator in confirm controller

--- api/src/modules/rides/controllers/confirm.controller.ts
+++ api/src/modules/rides/controllers/confirm.controller.ts
@@ -1,17 +1,24 @@
-import { BadRequestException, Body, Controller, Patch } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Patch,
+  UseFilters,
+} from '@nestjs/common';
 import { ClassSerializerInterceptor, UseInterceptors } from '@nestjs/common';
 import { ConfirmRequestDto } from '../dtos/confirm.request.dto';
 import { ConfirmService } from '../services/confirm.service';
 import { CodeErrorsEnum } from '@/protocols/code-errors.type';
 import { formatResponseError } from '@/utils/format-response-error.util';
+import { ClassValidatorExceptionFilter } from '@/exceptions/class-validator.exception';
 
 @Controller('rides')
 @UseInterceptors(ClassSerializerInterceptor)
 export class ConfirmController {
   constructor(private readonly confirmService: ConfirmService) {}
 
-  // TODO: Implement response schema for this endpoint equal documentation
   @Patch('confirm')
+  @UseFilters(new ClassValidatorExceptionFilter())
   async handle(@Body() body: ConfirmRequestDto) {
     if (body.origin === body.destination) {
       throw new BadRequestException(
