dd108ab0a4dc2c3e82a2bf09e72ce6211cda2ae2
Yuri Pinheiro <yuripinheirot@gmail.com>
2024-11-22 19:33:34 -0300

    feat: add validation do origin e destination

--- client/src/views/estimate/components/form/schema.ts
+++ client/src/views/estimate/components/form/schema.ts
@@ -3,8 +3,13 @@ import { z } from 'zod'
 
 export type EstimateFormSchemaType = z.infer<typeof EstimateFormSchema>
 
-export const EstimateFormSchema = z.object({
-  customer_id: z.string().min(1, { message: defaultMessages.required }),
-  origin: z.string().min(1, { message: defaultMessages.required }),
-  destination: z.string().min(1, { message: defaultMessages.required }),
-})
+export const EstimateFormSchema = z
+  .object({
+    customer_id: z.string().min(1, { message: defaultMessages.required }),
+    origin: z.string().min(1, { message: defaultMessages.required }),
+    destination: z.string().min(1, { message: defaultMessages.required }),
+  })
+  .refine((data) => data.origin !== data.destination, {
+    path: ['destination'],
+    message: 'Origem e destino não podem ser iguais',
+  })
