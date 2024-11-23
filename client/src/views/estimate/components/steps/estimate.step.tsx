4d5a94927a7789db2fcacae4873fd77806692ca1
Yuri Pinheiro <yuripinheirot@gmail.com>
2024-11-23 17:16:31 -0300

    feat: add integration with backend and markers

--- client/src/views/estimate/components/steps/estimate.step.tsx
+++ client/src/views/estimate/components/steps/estimate.step.tsx
@@ -2,16 +2,23 @@ import { SubmitHandler } from 'node_modules/react-hook-form/dist/types'
 import { EstimateForm } from '../forms/estimate'
 import { EstimateFormSchemaType } from '../forms/schema'
 import { GoogleMapPreview } from '../../google-map-preview'
+import { useMutation } from '@tanstack/react-query'
+import { ridesController } from '@/api/controllers/rides/rides.controller'
 
 export const EstimateStep = () => {
-  const onSubmit: SubmitHandler<EstimateFormSchemaType> = (data) => {
-    console.log(data)
+  const { mutateAsync: estimateRoute, data: estimatedRouteData } = useMutation({
+    mutationFn: (data: EstimateFormSchemaType) =>
+      ridesController.estimate(data),
+  })
+
+  const onSubmit: SubmitHandler<EstimateFormSchemaType> = async (data) => {
+    await estimateRoute(data)
   }
 
   return (
     <div className='flex flex-col gap-4'>
       <EstimateForm onSubmit={onSubmit} />
-      <GoogleMapPreview />
+      <GoogleMapPreview data={estimatedRouteData} />
     </div>
   )
 }
--- client/src/views/estimate/google-map-preview.tsx
+++ client/src/views/estimate/google-map-preview.tsx
@@ -1,9 +1,33 @@
+import { EstimateResponseType } from '@/api/controllers/rides/protocols/estimate.response.type'
 import { GoogleMap } from '@/components/google-map'
 
-export const GoogleMapPreview = () => {
+export const GoogleMapPreview = ({
+  data,
+}: {
+  data: EstimateResponseType | undefined
+}) => {
+  const locations = data
+    ? [
+        {
+          key: 'start',
+          location: {
+            lat: data.origin.latitude,
+            lng: data.origin.longitude,
+          },
+        },
+        {
+          key: 'end',
+          location: {
+            lat: data.destination.latitude,
+            lng: data.destination.longitude,
+          },
+        },
+      ]
+    : []
+
   return (
     <div className='h-[400px] w-full'>
-      <GoogleMap />
+      <GoogleMap markers={locations} />
     </div>
   )
 }
