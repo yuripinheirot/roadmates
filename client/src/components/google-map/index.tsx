24dcc2671564c1f352a754b152cb760066694f65
Yuri Pinheiro <yuripinheirot@gmail.com>
2024-11-23 17:15:52 -0300

    feat: add default values to google map

--- client/src/components/google-map/index.tsx
+++ client/src/components/google-map/index.tsx
@@ -1,13 +1,28 @@
 import { Map } from '@vis.gl/react-google-maps'
 import { Markers } from './marker'
 import { MarkerType } from './types'
+import { useEffect, useState } from 'react'
 
-export const GoogleMap = ({ markers }: { markers: MarkerType[] }) => {
+type Props = {
+  markers: MarkerType[]
+  defaultZoom?: number
+  defaultCenter?: { lat: number; lng: number }
+}
+
+export const GoogleMap = ({ markers, defaultZoom, defaultCenter }: Props) => {
+  const [zoom, setZoom] = useState(defaultZoom || 4)
+
+  useEffect(() => {
+    setZoom(4)
+  }, [markers])
+
   return (
     <Map
-      defaultZoom={10}
-      defaultCenter={{ lat: -23.533773, lng: -46.62529 }}
       mapId='afe811e4017a5a32'
+      zoom={zoom}
+      onZoomChanged={(zoom) => setZoom(zoom.detail.zoom)}
+      defaultZoom={defaultZoom || 4}
+      defaultCenter={defaultCenter || { lat: -14.235004, lng: -51.92528 }}
     >
       <Markers points={markers} />
     </Map>
