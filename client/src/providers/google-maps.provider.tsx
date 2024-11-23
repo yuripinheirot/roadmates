c2f329c43435badbb15320066ec35755899ee583
Yuri Pinheiro <yuripinheirot@gmail.com>
2024-11-23 13:55:31 -0300

    chore: add google maps provider

---
+++ client/src/providers/google-maps.provider.tsx
@@ -0,0 +1,16 @@
+import { APIProvider } from '@vis.gl/react-google-maps'
+
+export const GoogleMapsProvider = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
+  return (
+    <APIProvider
+      apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
+      onLoad={() => console.log('Maps API has loaded.')}
+    >
+      {children}
+    </APIProvider>
+  )
+}
--- client/src/providers/index.tsx
+++ client/src/providers/index.tsx
@@ -1,5 +1,10 @@
+import { GoogleMapsProvider } from './google-maps.provider'
 import { QueryClientProviderBase } from './query-client.provider'
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <QueryClientProviderBase>{children}</QueryClientProviderBase>
+  return (
+    <QueryClientProviderBase>
+      <GoogleMapsProvider>{children}</GoogleMapsProvider>
+    </QueryClientProviderBase>
+  )
 }
