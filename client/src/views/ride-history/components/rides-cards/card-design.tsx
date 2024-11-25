24138b59db460a5b327130f0084c194ca33814a3
Yuri Pinheiro <yuripinheirot@gmail.com>
2024-11-25 13:21:15 -0300

    feat: init rides card design

---
+++ client/src/views/ride-history/components/rides-cards/card-design.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+type Props = {
+  data: any
+}
+export const CardDesign = ({ data }: Props) => {
+  return <div>CardDesign</div>
+}
---
+++ client/src/views/ride-history/components/rides-cards/index.tsx
@@ -0,0 +1,14 @@
+import { DriverModel } from '@/domain/models/driver.model'
+import { CardDesign } from './card-design'
+
+type Props = {
+  data: DriverModel[]
+}
+
+export const RidesCards = ({ data }: Props) => {
+  const renderCards = () => {
+    return data.map((data) => <CardDesign data={data} />)
+  }
+
+  return <div className='flex flex-col gap-4'>{renderCards()}</div>
+}
