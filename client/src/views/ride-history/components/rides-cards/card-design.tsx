df918b3e4268ef9e69f40b9e471f40f560f9b0b7
Yuri Pinheiro <yuripinheirot@gmail.com>
2024-11-25 15:49:38 -0300

    refactor: small changes

--- client/src/views/ride-history/components/rides-cards/card-design.tsx
+++ client/src/views/ride-history/components/rides-cards/card-design.tsx
@@ -3,10 +3,8 @@ import {
   CardTitle,
   CardDescription,
   CardContent,
-  CardFooter,
   Card,
 } from '@/components/ui/card'
-import { Button } from '@/components/ui/button'
 import { Typography } from '@/components/ui/typography'
 import { RideModel } from '@/domain/models/ride.model'
 import { DineroUtils } from '@/utils/dinero'
@@ -46,8 +44,7 @@ export const CardDesign = ({ data }: { data: RideModel }) => {
         <CardTitle>
           {data.origin} - {data.destination}
         </CardTitle>
-        <CardDescription>{data.origin}</CardDescription>
-        <CardDescription>{data.destination}</CardDescription>
+        <CardDescription>Dados da corrida</CardDescription>
       </CardHeader>
       <CardContent>
         <div className='flex flex-col gap-2'>
@@ -84,9 +81,6 @@ export const CardDesign = ({ data }: { data: RideModel }) => {
           />
         </div>
       </CardContent>
-      <CardFooter className='flex justify-end'>
-        <Button>Escolher</Button>
-      </CardFooter>
     </Card>
   )
 }
